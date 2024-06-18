cfe4fd1709521b50ae1ca47582541c01622fe150
MAHENDRA KUMAR SUDDAPALLI <132595641+Mahendrakumar0109@users.noreply.github.com>
2024-06-18 22:21:13 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Weather from './Weather';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Weather App</h1>
+      </header>
+      <main>
+        <Weather />
+      </main>
+    </div>
+  );
+}
+
+export default App;
